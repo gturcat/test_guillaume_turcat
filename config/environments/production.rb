b687f0f985c6851e206e7d83dca3a2e5e4ba516b
guillaume turcat <gturcat@me.com>
2019-07-22 20:19:57 +0200

    configuration email production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "http://localhost:3000/" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: "https://testguillaumeturcat.herokuapp.com" }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
