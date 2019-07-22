4da0bc54982198ab2f00254932db9af2082f6280
guillaume turcat <gturcat@me.com>
2019-07-23 00:34:07 +0200

    ajout page home

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'requests#new'
+  root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :requests, only: [:new, :create, :show] do
     member do
