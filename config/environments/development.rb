ce4ddec607fac5f44cf5c79f2b0ed0914bd215e8
guillaume turcat <gturcat@me.com>
2019-07-26 09:34:45 +0200

    done

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   # Verifies that versions and hashed value of the package contents in the project's package.json
   config.webpacker.check_yarn_integrity = true
   config.action_mailer.default_url_options = { host: "http://localhost:3000" }
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :letter_opener
 
   # Settings specified here will take precedence over those in config/application.rb.
 
