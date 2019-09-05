5e11b14330bae4908a3f7600f89e30d10d0f8f31
guillaume turcat <gturcat@me.com>
2019-09-05 10:09:25 +0200

    passage des requetes en arel

--- app/validators/availability_validator.rb
+++ app/validators/availability_validator.rb
@@ -1,7 +1,8 @@
 class AvailabilityValidator < ActiveModel::EachValidator
 
   def validate_each(record, attribute, value)
-    bookings = Booking.where(["desk_id =?", record.desk_id])
+    # bookings = Booking.where(["desk_id =?", record.desk_id])
+    bookings = Booking.select(Arel.star).where(Booking.arel_table[:desk_id].eq(record.desk_id))
     date_ranges = bookings.map { |b| b.start_date..b.end_date }
 
     date_ranges.each do |range|
