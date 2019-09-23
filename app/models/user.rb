7d9762e4cbfb82215931d669bbecf53f263b6182
guillaume turcat <gturcat@me.com>
2019-09-23 10:10:11 +0200

    suppression de name à la cration du user

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,8 +22,7 @@ class User < ApplicationRecord
 
     # Uncomment the section below if you want users to be created if they don't exist
     unless user
-        user = User.create(name: data["name"],
-           email: data["email"],
+        user = User.create(email: data["email"],
            password: Devise.friendly_token[0,20]
         )
     end
