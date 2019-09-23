209bb179353ab678f54e841b59230c83eb02c874
guillaume turcat <gturcat@me.com>
2019-09-23 10:02:35 +0200

    done

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable, :omniauth_providers => [:google_oauth2]
   has_many :bookings
 
-  devise :omniauthable, :omniauth_providers => [:google_oauth2]
 
    def self.from_omniauth(auth)
     # Either create a User record or update it based on the provider (Google) and the UID
@@ -15,6 +14,20 @@ class User < ApplicationRecord
       user.expires_at = auth.credentials.expires_at
       user.refresh_token = auth.credentials.refresh_token
     end
+  end
+
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.where(:email => data["email"]).first
+
+    # Uncomment the section below if you want users to be created if they don't exist
+    unless user
+        user = User.create(name: data["name"],
+           email: data["email"],
+           password: Devise.friendly_token[0,20]
+        )
+    end
+    user
   end
 
 end
