084f6a36a7be4d0f8ff2cad37ee229a3c5ed29b7
guillaume turcat <gturcat@me.com>
2019-09-23 09:40:32 +0200

    add route modificiation

--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,12 @@ Rails.application.routes.draw do
     end
   end
 
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-
+  # devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users, :controllers => {
+    :omniauth_callbacks => "users/omniauth_callbacks"
+  }
+  devise_scope :user do
+    get 'sign_in', :to => 'devise/sessions#new', :as => :new_user_session
+    delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
 end
